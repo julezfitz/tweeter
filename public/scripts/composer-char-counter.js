306ba5824e8cf47abccca960df20c26ef2b3c8d3
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-09-10 00:59:59 +0000

    refractoring composer0char-counter.js to jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,8 @@
 
-$(document).ready(function () {
-  const tweetText = document.getElementById("tweet-text");
-  tweetText.addEventListener("input", function (event) {
+$(function () {
+  const tweetText = $("#tweet-text");
+  tweetText.on("input", function (event) {
     let countVal = 140 - $(this).val().length;
-    //let form = $(this).parent();
     let formLowDiv = $(this).parent().children(".bottomTweet");
     formLowDiv.children(".counter").val(countVal);
 
