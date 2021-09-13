bdf977cd6b7caec7d9ec509053946f449144f3d3
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-09-13 22:59:14 +0000

    function to loop through array of tweets and append them to tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,21 +1,35 @@
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-const createTweetElement = function(data) {
+const createTweetElement = function (data) {
   let timeStamp = timeago.format(data.created_at);
 
   const $tweet =
- $(`<article class="tweet">
+    $(`<article class="tweet">
 
  <header>
          <div class = "username-pic">
@@ -37,13 +51,19 @@ const createTweetElement = function(data) {
         </footer>
  </article>`);
 
-  const tweetContain = $('.tweet-container');
+  return $tweet;
+};
 
-  return tweetContain.append($tweet);
+const renderTweets = function (tweetArray) {
+  const tweetContain = $('.tweet-container');
+  for (const key in tweetArray) {
+    let $tweet = createTweetElement(tweetArray[key]);
+    tweetContain.append($tweet);
+  }
 };
 
 $(function () {
-
-  let tweetButton = $('.tweetButton');
-  createTweetElement(tweetData);
+  const tweetButton = $('.tweetButton');
+  //$(tweetButton).click()
+  renderTweets(data);
 });
