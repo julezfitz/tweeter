1f0a791c4147246e321f997d1f15aa07453fe745
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-09-13 22:45:59 +0000

    adds function to create tweet from a dataset

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,49 @@
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = function(data) {
+  let timeStamp = timeago.format(data.created_at);
+
+  const $tweet =
+ $(`<article class="tweet">
+
+ <header>
+         <div class = "username-pic">
+         <img class = "profile-pic" src = ${data.user.avatars}>
+         <p>${data.user.name}</p>
+         </div>
+         <p class = "username">${data.user.handle}</p>
+        </header> 
+
+        <p class = "tweet-text">${data.content.text}</p>
+        <hr>
+        <footer>
+          <p class = "time">${timeStamp}</p>
+          <div class = "social">
+            <i class="fas fa-solid fa-flag"></i>
+            <i class="fas fa-solid fa-retweet"></i>
+            <i class="fas fa-solid fa-heart"></i>
+          </div>
+        </footer>
+ </article>`);
+
+  const tweetContain = $('.tweet-container');
+
+  return tweetContain.append($tweet);
+};
+
 $(function () {
-  const render = document.querySelector('.need_to_be_rendered');
-  const time = timeago.format(new Date());
-  render.innerHTML += time;
+
+  let tweetButton = $('.tweetButton');
+  createTweetElement(tweetData);
 });
