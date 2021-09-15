8838dc84d893fb5db03ef552aa231733284b8bc6
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-09-15 12:38:37 +0000

    makes up button appear on scrolling down

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -63,6 +63,16 @@ const errorMessage = function (error) {
 $(() => {
   loadTweets();
 
+  //handles scroll to top button appearance
+  $(document).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.upArrow:hidden').stop(true, true).fadeIn();
+    } else {
+      $('.upArrow').stop(true, true).fadeOut();
+    }
+  });
+
+  //toggles the view of the create tweet div using the 'write a new tweet' area
   $('.rightNav').click(() => {
     if ($('.new-tweet').is(":hidden")) {
       $('.new-tweet').slideDown("fast");
